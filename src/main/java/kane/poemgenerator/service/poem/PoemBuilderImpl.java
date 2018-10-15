4b5e7e21dadf7a399a73b4ef2d78ae15aa95ddeb
Vikas Saini <vikas.saini@aeris.net>
2018-10-16 03:01:23 +0530

    First commit for poem generator

--- src/main/java/kane/poemgenerator/service/poem/PoemBuilderImpl.java
+++ src/main/java/kane/poemgenerator/service/poem/PoemBuilderImpl.java
@@ -3,10 +3,12 @@ package kane.poemgenerator.service.poem;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 import java.util.Random;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,19 +20,21 @@ import kane.poemgenerator.service.rules.Rule;
 public class PoemBuilderImpl implements PoemBuilder {
 
 	private static final Logger logger = LoggerFactory.getLogger(PoemBuilderImpl.class);
-	
+
 	private static final String KEY_ARG_TYPE_PATTERN_ONE = "<[A-Z]+>";
 	private static final String KEY_ARG_TYPE_PATTERN_TWO = "\\$[A-Z]+";
 	private static FileWriter fr = null;
-	private static  BufferedWriter br = null;
+	private static BufferedWriter br = null;
 
 	public void buildPoem(Rules rules) {
 		try {
 			StringBuilder sb = new StringBuilder();
 			LinkedHashMap<String, Rule> poemRules = (LinkedHashMap<String, Rule>) rules.getRuleLines();
 			if (!poemRules.isEmpty()) {
-				for (Entry<String, Rule> entry : poemRules.entrySet()) {
-					generatePoem((PoemRule) entry.getValue(), poemRules, sb);
+				PoemRule mainRule = (PoemRule) poemRules.get("POEM");
+				ArrayList<String> mainRuleArgs = (ArrayList<String>) mainRule.getKeyArgs();
+				for (String mainRuleArg : mainRuleArgs) {
+					generatePoem((PoemRule) poemRules.get(mainRuleArg.substring(1, mainRuleArg.length() - 1)), poemRules, sb);
 				}
 			}
 			System.out.println(sb.toString());
@@ -40,7 +44,8 @@ public class PoemBuilderImpl implements PoemBuilder {
 		}
 	}
 
-	private void generatePoem(PoemRule poemRule, LinkedHashMap<String, Rule> poemRules, StringBuilder sb) throws Exception {
+	private void generatePoem(PoemRule poemRule, LinkedHashMap<String, Rule> poemRules, StringBuilder sb)
+			throws Exception {
 		try {
 			if (!poemRule.getBasicArgs().isEmpty()) {
 				sb.append(poemRule.getBasicArgs().get(randomIndex(0, poemRule.getBasicArgs().size())));
@@ -63,7 +68,7 @@ public class PoemBuilderImpl implements PoemBuilder {
 		Random r = new Random();
 		return r.nextInt(max - min) + min;
 	}
-	
+
 	private void writeOutput(String output) {
 		File outputFile = new File("results.txt");
 		try {
